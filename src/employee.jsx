0fd22e52fee3a0553cc891884eafc7ea794beeab
Chris357 <Chris@eup.co.th>
2026-05-29 13:29:59 +0700

    update monthlyschedule

--- src/employee.jsx
+++ src/employee.jsx
@@ -1,9 +1,4 @@
 const employees = [
-  { id: 1, name: "Mook" },
-  { id: 2, name: "Rose" },
-  { id: 3, name: "Moss" },
-  { id: 4, name: "Bella" },
-  { id: 6, name: "Sky" },
   { id: 5, name: "Lucus" },
   { id: 7, name: "Hut" },
   { id: 8, name: "Christian" },
@@ -12,6 +7,11 @@ const employees = [
   { id: 11, name: "Ethan" },
   { id: 12, name: "Chris" },
   { id: 13, name: "Day" },
+  { id: 1, name: "Mook" },
+  { id: 2, name: "Rose" },
+  { id: 3, name: "Moss" },
+  { id: 4, name: "Smile" },
+  { id: 6, name: "Sky" },
 ];
 
 export default employees;
