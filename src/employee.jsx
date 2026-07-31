30408dcbf11c5b7da17c5740606ef4baaf1379a6
Chris357 <Chris@eup.co.th>
2026-07-31 14:46:26 +0700

    change_employee_Position

--- src/employee.jsx
+++ src/employee.jsx
@@ -1,4 +1,9 @@
 const employees = [
+  { id: 9, name: "Min" },
+  { id: 10, name: "Yok" },
+  { id: 11, name: "Ethan" },
+  { id: 12, name: "Chris" },
+  { id: 13, name: "Day" },
   { id: 1, name: "Mook" },
   { id: 2, name: "Rose" },
   { id: 3, name: "Moss" },
@@ -7,11 +12,6 @@ const employees = [
   { id: 5, name: "Lucus" },
   { id: 7, name: "Hut" },
   { id: 8, name: "Christian" },
-  { id: 9, name: "Min" },
-  { id: 10, name: "Yok" },
-  { id: 11, name: "Ethan" },
-  { id: 12, name: "Chris" },
-  { id: 13, name: "Day" },
 ];
 
 export default employees;
